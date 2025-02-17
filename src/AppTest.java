c7fa698fd2902af0b1376beff0e219034a216fa0
JosueSay <106031855+JosueSay@users.noreply.github.com>
2025-02-16 21:38:38 -0600

    feat: plantilla base para proyecto java

---
+++ src/AppTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testSayHello() {
+    assertEquals("Hello, World!", App.sayHello());
+  }
+
+}
